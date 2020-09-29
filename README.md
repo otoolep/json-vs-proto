2b866adc782ef2251d41991cfbcfeb4002857f5a
Philip O'Toole <philip.otoole@yahoo.com>
2020-09-29 13:48:39 -0400

    More experimentation
    Need to test compressing the proto better. Not clear if it should be
    better, or is not.

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ export SRC_DIR=/home/philip/repos/rqlite/src/github.com/rqlite/rqlite/store/prot
 export DEST_DIR=/home/philip/repos/rqlite/src/github.com/rqlite/rqlite/store/proto
 
 protoc -I=$SRC_DIR --go_out=$DEST_DIR $SRC_DIR/command.proto
+
+Findings:
+- Proto is always a win, the smaller the query, the bigger the win (JSON is a bigger overhead in that case)
+- Compression more likely to be a win the more statements (>5?) there are OR if one of the queries is very long (100 characters?).
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"log"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/otoolep/json-vs-proto/chinook"
+	//"github.com/otoolep/json-vs-proto/chinook"
 	"github.com/otoolep/json-vs-proto/command"
 )
 
@@ -22,18 +22,21 @@ type POD struct {
 func main() {
 	fmt.Println("Compare sizes....")
 
+	//q1 := []string{"SELECT column1, column2 FROM table1 WHERE column3 IN ( SELECT TOP(1) column4 FROM table2 INNER JOIN table3 ON table2.column1 = table3.column1)"}
 	//q1 := []string{"INSERT INTO foo(name, age, place, booked) VALUES(fiona, 20, GLENMORE, true)"}
+	//q1 := []string{"INSERT INTO foo(name, age, place, booked) VALUES(fiona, 20, GLENMORE, true)", "SELECT column1, column2 FROM table1 WHERE column3 IN ( SELECT TOP(1) column4 FROM table2 INNER JOIN table3 ON table2.column1 = table3.column1)"}
 	//q1 := []string{"SELECT * FROM foo", "INSERT INTO foo(name, age) VALUES(fiona, 20)", "INSERT INTO foo(name, age) VALUES(dana, 44)"}
 	//q1 := []string{"INSERT INTO foo(name, age) VALUES(fiona, 20", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo"}
-	q1 := []string{chinook.DB}
+	q1 := []string{"SELECT * FROM foo", "SELECT column1, column2 FROM table1 WHERE column3 IN ( SELECT TOP(1) column4 FROM table2 INNER JOIN table3 ON table2.column1 = table3.column1)", "INSERT INTO foo(name, age, place, booked) VALUES(fiona, 20, GLENMORE, true)"}
+	//q1 := []string{chinook.DB}
 
+	// Proto only.
 	p := &command.QueryCommand{
 		Timings:     true,
 		Transaction: false,
 		Query:       q1,
 		Value:       nil,
 	}
-
 	pb, err := proto.Marshal(p)
 	if err != nil {
 		log.Fatalf("failed to marshal protobuf: %s", err.Error())
@@ -41,13 +44,13 @@ func main() {
 	pl := len(pb)
 	fmt.Println("Proto:", pl)
 
+	// JSON only.
 	j := &POD{
 		Timings:     true,
 		Transaction: false,
 		Query:       q1,
 		Value:       nil,
 	}
-
 	jb, err := json.Marshal(j)
 	if err != nil {
 		log.Fatalf("failed to marshal JSON: %s", err.Error())
@@ -55,9 +58,22 @@ func main() {
 	jl := len(jb)
 	fmt.Println("JSON:", jl)
 
-	fmt.Println(float64((jl-pl))/float64(jl)*100, "% reduction.")
-
+	// Compressed JSON
 	var buf bytes.Buffer
+	gz, err := gzip.NewWriterLevel(&buf, gzip.BestCompression)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.NewEncoder(gz).Encode(j); err != nil {
+		log.Fatalf("failed to encode and gzip JSON: %s", err.Error())
+	}
+	gz.Close()
+	lbuf := buf.Len()
+	fmt.Println("Compressed JSON:", lbuf)
+
+	fmt.Print(int(float64((jl-pl))/float64(jl)*100), "% reduction moving from JSON to Proto.\n")
+	fmt.Print(int(float64((jl-lbuf))/float64(jl)*100), "% reduction moving from JSON to compressed JSON.\n")
+
 	zw, err := gzip.NewWriterLevel(&buf, gzip.BestCompression)
 	if err != nil {
 		log.Fatal(err)
