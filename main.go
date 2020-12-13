bba9872eae45048c3cc51b3b887d13325c4d7472
Philip O'Toole <philip.otoole@yahoo.com>
2020-12-13 12:11:08 -0500

    Show how to set oneof

--- main.go
+++ main.go
@@ -26,16 +26,25 @@ func main() {
 	//q1 := []string{"INSERT INTO foo(name, age, place, booked) VALUES(fiona, 20, GLENMORE, true)"}
 	//q1 := []string{"INSERT INTO foo(name, age, place, booked) VALUES(fiona, 20, GLENMORE, true)", "SELECT column1, column2 FROM table1 WHERE column3 IN ( SELECT TOP(1) column4 FROM table2 INNER JOIN table3 ON table2.column1 = table3.column1)"}
 	//q1 := []string{"SELECT * FROM foo", "INSERT INTO foo(name, age) VALUES(fiona, 20)", "INSERT INTO foo(name, age) VALUES(dana, 44)"}
-	//q1 := []string{"INSERT INTO foo(name, age) VALUES(fiona, 20", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo"}
-	q1 := []string{"SELECT * FROM foo", "SELECT column1, column2 FROM table1 WHERE column3 IN ( SELECT TOP(1) column4 FROM table2 INNER JOIN table3 ON table2.column1 = table3.column1)", "INSERT INTO foo(name, age, place, booked) VALUES(fiona, 20, GLENMORE, true)"}
+	//q1 := []string{`SELECT * FROM "foo"`, "SELECT * FROM foo", "SELECT * FROM bar", "SELECT * FROM qux"}
+	q1 := []string{"INSERT INTO foo(name, ag) VALUES(fiona, 20", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo", "SELECT * FROM foo"}
+	//q1 := []string{"SELECT * FROM foo", "SELECT column1, column2 FROM table1 WHERE column3 IN ( SELECT TOP(1) column4 FROM table2 INNER JOIN table3 ON table2.column1 = table3.column1)", "INSERT INTO foo(name, age, place, booked) VALUES(fiona, 20, GLENMORE, true)"}
 	//q1 := []string{chinook.DB}
 
+	v0 := []interface{}{1, "qux"}
+	v1 := [][]interface{}{v0}
+
 	// Proto only.
+	pv1 := &command.Parameter{
+		Value: &command.Parameter_I{
+			I: 1,
+		},
+	}
 	p := &command.QueryCommand{
 		Timings:     true,
 		Transaction: false,
 		Query:       q1,
-		Value:       nil,
+		Value:       []*command.Parameter{pv1},
 	}
 	pb, err := proto.Marshal(p)
 	if err != nil {
@@ -44,12 +53,25 @@ func main() {
 	pl := len(pb)
 	fmt.Println("Proto:", pl)
 
+	// Compressed proto
+	var cp bytes.Buffer
+	gzp, err := gzip.NewWriterLevel(&cp, gzip.BestCompression)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.NewEncoder(gzp).Encode(pb); err != nil {
+		log.Fatalf("failed to compress marshalled proto: %s", err.Error())
+	}
+	gzp.Close()
+	lcp := cp.Len()
+	fmt.Println("Compressed marshalled proto", lcp)
+
 	// JSON only.
 	j := &POD{
 		Timings:     true,
 		Transaction: false,
 		Query:       q1,
-		Value:       nil,
+		Value:       v1,
 	}
 	jb, err := json.Marshal(j)
 	if err != nil {
@@ -71,18 +93,30 @@ func main() {
 	lbuf := buf.Len()
 	fmt.Println("Compressed JSON:", lbuf)
 
+	// Encode SQL as JSON, compress, add to proto.
+	var bufq bytes.Buffer
+	gzq, err := gzip.NewWriterLevel(&bufq, gzip.BestCompression)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := json.NewEncoder(gzq).Encode(q1); err != nil {
+		log.Fatalf("failed to encode as JSON and compress queries: %s", err.Error())
+	}
+	gzq.Close()
+	pq := &command.QueryCommand{
+		Timings:         true,
+		Transaction:     false,
+		CompressedQuery: bufq.Bytes(),
+	}
+	pbq, err := proto.Marshal(pq)
+	if err != nil {
+		log.Fatalf("failed to marshal protobuf: %s", err.Error())
+	}
+	jql := len(pbq)
+	fmt.Println("Compressed JSON in proto:", jql)
+
 	fmt.Print(int(float64((jl-pl))/float64(jl)*100), "% reduction moving from JSON to Proto.\n")
+	fmt.Print(int(float64((jl-lcp))/float64(jl)*100), "% reduction moving from JSON to compressed marshalled Proto.\n")
 	fmt.Print(int(float64((jl-lbuf))/float64(jl)*100), "% reduction moving from JSON to compressed JSON.\n")
-
-	zw, err := gzip.NewWriterLevel(&buf, gzip.BestCompression)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = zw.Write(pb)
-	if err != nil {
-		log.Fatal(err)
-	}
-	zw.Close()
-	fmt.Println("Buffer length:", buf.Len())
+	fmt.Print(int(float64((jl-jql))/float64(jl)*100), "% reduction moving from JSON to compressed JSON queries in Proto.\n")
 }
